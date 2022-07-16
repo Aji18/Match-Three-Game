d108e328c45890ad30e2c964bdb796b28b43bbe6
Sulton Aji Kurniawan <36285397+Aji18@users.noreply.github.com>
2022-07-16 13:03:51 +0700

    Upload Match Three

---
+++ Match Three/Assets/Scripts/Row.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace MatchThree
+{
+	public sealed class Row : MonoBehaviour
+	{
+		public Tile[] tiles;
+	}
+}
---
+++ Match Three/Assets/Scripts/TileTypeAsset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MatchThree
+{
+	[CreateAssetMenu(menuName = "Match Three/Tile Type Asset")]
+	public sealed class TileTypeAsset : ScriptableObject
+	{
+		public int id;
+
+		public int value;
+
+		public Sprite sprite;
+	}
+}
